c40b1921bf1fc1b2b3cfd69cd6785789bfd525dc
TTT <18513228355@163.com>
2023-02-16 15:50:20 +0800

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    这是首页
+    <Tabbar></Tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from "../components/common/Tabbar.vue";
+export default {
+  name: "Home",
+  components: {
+    Tabbar,
+  },
+};
+</script>
